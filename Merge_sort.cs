b4d6f3cb979ad12d29bc1d5042eb72569bcffd1a
GMfitundnda <mfitundinda.gilbert@gmail.com>
2021-03-01 10:50:12 -0600

    Sorts arrays using a merge sort algorithm

--- Merge_sort.cs
+++ Merge_sort.cs
@@ -8,7 +8,7 @@ class MergeSort
    
 {
 
-    // Merges two subarrays of []arr.
+    // Merging two subarrays of []arr.
     // First subarray is arr[l..m]
     // Second subarray is arr[m+1..r]
     void merge(int[] arr, int l, int m, int r)
