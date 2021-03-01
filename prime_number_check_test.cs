0408419ea0c7450e1bf08b372e252dd893321ec1
GMfitundnda <mfitundinda.gilbert@gmail.com>
2021-03-01 12:13:28 -0600

    edited comment

--- prime_number_check_test.cs
+++ prime_number_check_test.cs
@@ -2,7 +2,7 @@
 //March/01/2021
 
 [TestMethod]
-//the method would test the returned value to see if it is true or not.
+//the method would test the returned value to see if it is true or false.
 public void ()
 {
     Assert.AreEqual(flag, 1, "Positive integer entered is a prime Number");
